ce48872f0150d4a5722e84e4f27e7f97b5574dc2
Alex Aguiar <alex3aguiar@gmail.com>
2023-07-02 04:40:59 -0300

    s

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-  
+    output: 'export',
+    
     distDir: '/github-pages',
     trailingSlash: true,
     basePath: '/github-pages',
